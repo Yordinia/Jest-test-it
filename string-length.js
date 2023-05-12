caca9e13b6da5d7dcf1e360b32a9581d0dde4946
Yordi <yorditjordin@gmail.com>
2023-05-12 06:41:25 +0300

    Update string-length.js

--- string-length.js
+++ string-length.js
@@ -1,7 +1,22 @@
+let y = 0;
+let ths = [null,'st','nd','rd','th']
 const stringLength = string => {
-    let x = string.trim().length;
-    let sol = (x>0 && x<=10)? x : 'error';
-    return sol;
+  y += 1;
+  let subfix = (y>3)? y+ths[4]: y+ths[y];
+  console.log('========================================')
+  console.log('this is the', subfix, 'time stringLength runs');
+  let x = string.trim().length;
+  if(x>0 && x<=10) {"Do Nothing";
+  console.log('x>0 && x<=10 is: ', x>0 && x<=10, x);
+    }
+  else {
+    console.log('x>0 && x<=10 is: ', x>0 && x<=10, x);
+    console.error('not  b/n 1&10 chars'); throw Error}
+    return x;
   }
   
+  console.log(stringLength('halla  '))
+
+  console.log(stringLength(' hallahallahallahallahalla'))
+
   module.exports = stringLength;
