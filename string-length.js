8cce177b27e40298dc582f2244b6a8f5627ac9dc
Yordi <yorditjordin@gmail.com>
2023-05-13 12:11:36 +0300

    sum

--- string-length.js
+++ string-length.js
@@ -4,19 +4,13 @@ const stringLength = string => {
   y += 1;
   let subfix = (y>3)? y+ths[4]: y+ths[y];
   console.log('========================================')
-  console.log('this is the', subfix, 'time stringLength runs');
+  console.log('this is the', subfix, ' time stringLength runs');
   let x = string.trim().length;
-  if(x>0 && x<=10) {"Do Nothing";
-  console.log('x>0 && x<=10 is: ', x>0 && x<=10, x);
-    }
+  if(x>0 && x<=10);
   else {
-    console.log('x>0 && x<=10 is: ', x>0 && x<=10, x);
-    console.error('not  b/n 1&10 chars'); throw Error}
-    return x;
+    x = new Error('This is a BAAAD error');
   }
-  
-  console.log(stringLength('halla  '))
+  return x;
+}
 
-  console.log(stringLength(' hallahallahallahallahalla'))
-
-  module.exports = stringLength;
+module.exports = stringLength;
--- string-length.test.js
+++ string-length.test.js
@@ -1,6 +1,6 @@
 const reverseString = require('./string-length.js')
 test('Display string length b/n 1 and 10', () => {
     expect(reverseString("1, 2")).toBe(4);
-    expect(reverseString("            ")).toBe('error');
-    expect(reverseString(" mylovelygirl")).toBe(12);
+    expect(reverseString("            ")).toEqual(new Error('This is a BAAAD error'));
+    expect(reverseString(" mylvlygirl")).toBe(10);
   });
