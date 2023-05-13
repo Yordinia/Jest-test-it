15eb610f91af3e0713adeb53b9f524bfc046661f
Yordi <yorditjordin@gmail.com>
2023-05-13 12:19:40 +0300

    ds

--- reverseString.test.js
+++ reverseString.test.js
@@ -3,4 +3,6 @@ const reverseString = require('./reverseString');
 
 test('String is reversed correctly', () => {
   expect(reverseString('hello')).toBe('olleh');
+  expect(reverseString(true)).toThrow(TypeError);
+
 });
